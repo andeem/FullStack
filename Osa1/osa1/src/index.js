58aa96362d615146764f5fc3195d19ef7cfbdc94
Emil Andersson <anderssone0@gmail.com>
2018-01-16 15:37:20 +0200

    Tehtävät 1.5

--- Osa1/osa1/src/index.js
+++ Osa1/osa1/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 
 const Otsikko = (props) => {
     return (
-        <h1>{props.otsikko}</h1>
+        <h1>{props.kurssi.nimi}</h1>
     )
 }
 
 const Sisalto = (props) => {
-    let lista = props.tehtavat.map(function (item) {
-        return <Osa osa={item}/>
+    let lista = props.kurssi.osat.map(function (item) {
+        return <Osa osa={item} />
     })
     return lista
 }
@@ -22,8 +22,8 @@ const Osa = (props) => {
 
 const Yhteensa = (props) => {
     let maara = 0
-    for (let i = 0; i < props.tehtavat.length; i++) {
-        maara += props.tehtavat[i].tehtavia
+    for (let i = 0; i < props.kurssi.osat.length; i++) {
+        maara += props.kurssi.osat[i].tehtavia
     }
     return (
         <p>Yhteensä {maara} tehtävää</p>
@@ -32,18 +32,37 @@ const Yhteensa = (props) => {
 }
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const tehtavat = [{ id: 1, osa: "Reactin perusteet", tehtavia: 10 }, { id: 2, osa: "Tiedonvälitys propseilla", tehtavia: 7 }, { id: 3, osa: "Komponenttien tila", tehtavia: 14 }]
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                id: 1,
+                osa: "Reactin perusteet",
+                tehtavia: 10
+            },
+            {
+                id: 2,
+                osa: "Tiedonvälitys propseilla",
+                tehtavia: 7
+            },
+            {
+                id: 3,
+                osa: "Komponenttien tila",
+                tehtavia: 14
+            }
+
+        ]
+    }
     return (
         <div>
-            <Otsikko otsikko={kurssi} />
-            <Sisalto tehtavat={tehtavat} />
-            <Yhteensa tehtavat={tehtavat} />
+            <Otsikko kurssi={kurssi} />
+            <Sisalto kurssi={kurssi} />
+            <Yhteensa kurssi={kurssi} />
         </div>
     )
 }
 
 ReactDOM.render(
-    <App />, 
+    <App />,
     document.getElementById('root')
 )
